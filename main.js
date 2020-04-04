bc7f1ad6f611874c0c77dd3d3b2cd540eb6ef4be
GianmarcoMontanari <giammont@live.it>
2020-04-04 02:10:09 +0200

    selezione quadrati

--- main.js
+++ main.js
@@ -4,65 +4,67 @@
 $(document).ready(function(){
 
 
-
   // creo la variabile per la lista di 15 numeri random
   var listaNumeriSquare = [];
   // creo un ciclo con cui trovo 15 numeri random tra 1 e 64
   for (i = 0; i < 15; i++) {
+
+    // while(....) {
       // IMPOSTO UNA VARIABILE CHE MI TROVA UN NUMERO RANDOM TRA 1 E 64
       var random = Math.floor(Math.random() * 64);
       listaNumeriSquare.push(random);
-  }
+    }
+  
+
+  // while() {
+  //   ...
+  // }
   // con un ciclo for seleziono i quadrati partendo dalla lista di numeri random e li coloro di rosso
+  $(".square").addClass("sara_green");
 
   for (i = 0; i < listaNumeriSquare.length; i++) {
       var elementoSelezionato = $(".square").eq(listaNumeriSquare[i]);
-      elementoSelezionato.addClass("red");
-
+      elementoSelezionato.removeClass("sara_green");
+      elementoSelezionato.addClass("sara_red");
   }
 
-  //  con un altro ciclo for seleziono i quadratini non colorati di rosso e li coloro di verde
-  for (i = 0; i < 64; i++) {
-    var quadrato = $(".square").eq([i]);
+/*
+  $(".square").click(function() {
 
-    if ($(quadrato).hasClass("red")){
-      console.log("è rosso");
+
+
+    // quando siamo dentro la funzione chiamata dall'evento (che in questo caso è il clikc)
+
+    // this è l'elemento che ha scatenato l'evento
+
+
+    if ($(this).hasClass("sara_green")) {
+
+      $(this).addClass("green");
+
     } else {
-      $(quadrato).addClass("green");
+
+      $(this).addClass("red");
     }
-  }
 
 
+  });
+  */
 
+  $(".square").click(function() {
+    var quadratoPremuto = $(this).index();
+    if (listaNumeriSquare.includes(quadratoPremuto)){
+      $(this).addClass("red");
+    } else {
+      $(this).addClass("green");
+    }
+  });
 
+  // generazione quadratini
+  // per ogni quadrato decidere se è rosso o verde.
+  // se questo è rosso gli diamo una classe "informativa" per sapere che quando è cliccato deve colorarsi di rosso
+  // se questo è verde blablabla
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  $("square").click(
-    // funzione
-  )
-
-
-  function colora(){
-
-  }
+  // al click del quadrato verifico quale delle due classi ha e gli applico la relativa classe di sfondo
 
 });
-
-
-// for (i = 0, i < 15, i++) {
-//   var numeroSquare = random;
-//   console.log(numeroSquare);
-// }
